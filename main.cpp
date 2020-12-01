983c1360002f6fa595b3b4887c8500e238ef22c5
kirtish16 <kirtish16@gmail.com>
2020-12-01 22:27:52 +0530

    All instructions coded

--- main.cpp
+++ main.cpp
@@ -4,9 +4,11 @@ using namespace std;
 
 void getdata(string n[MAX],char a[10][10][4],int dataInd,int endInd,int blockInd);
 void putData(string n[MAX],char a[10][10][4],int dataInd,int endInd,int blockInd);
-void loadRegister(string n[MAX],char a[10][10][4],char reg[4],int dataInd,int endInd,int blockInd);
-void storeRegister(string n[MAX],char a[10][10][4],char reg[4],int dataInd,int endInd,int blockInd);
-void run(string n[MAX],char a[10][10][4],int jobInd,int dataInd,int endInd);
+void loadRegister(string n[MAX],char a[10][10][4],char reg[4],int dataInd,int endInd);
+void storeRegister(string n[MAX],char a[10][10][4],char reg[4],int dataInd,int endInd);
+int compareRegister(char a[10][10][4],char reg[4],int addr,int dataInd,int endInd);
+void branchTrue();
+void run(string n[MAX],char a[10][10][4],int numInstr,int jobInd,int dataInd,int endInd);
 void machine(string n[MAX],int len);
 
 int main()
@@ -27,6 +29,19 @@ int main()
     foo.close();
     return 0;
 }
+
+int compareRegister(char a[10][10][4],char reg[4],int addr,int dataInd,int endInd,int blockInd)
+{
+    int i,j=addr/10,k=addr%10;
+    for(i=0; i<4; i++)
+    {
+
+        if(reg[i] != a[j][k][i])
+            return 0;
+    }
+    return 1;
+}
+
 void loadRegister(string n[MAX],char a[10][10][4],char reg[4],int addr,int dataInd,int endInd)
 {
     int i,j=addr/10,k=addr%10;
@@ -47,11 +62,10 @@ void storeRegister(string n[MAX],char a[10][10][4],char reg[4],int addr,int data
 
 void getdata(string n[MAX], char a[10][10][4],int dataInd,int endInd,int blockInd)
 {
+    //while(dataInd < endInd)
     int t=dataInd+1,data_len,i=0,j=0,k=0;
     data_len = n[t].length();
-    cout<<"Data leng: "<<data_len;
     blockInd = blockInd / 10;
-    //cout<<"BlockInd :"<<blockInd;
 
     while(i<data_len)
     {
@@ -66,13 +80,10 @@ void getdata(string n[MAX], char a[10][10][4],int dataInd,int endInd,int blockIn
         i++;
     }
 
-    //cout<<"\nBlockind + 0 + 1:"<<a[blockInd][0][1];
 }
 
 void putData(string n[MAX], char a[10][10][4],int dataInd,int endInd,int blockInd)
 {
-    //cout<<"Data is loading ... "<<endl;
-    //while(dataInd < endInd)
     int i=0,j;
     blockInd = blockInd / 10;
 
@@ -86,54 +97,64 @@ void putData(string n[MAX], char a[10][10][4],int dataInd,int endInd,int blockIn
 
 }
 
-void run(string n[MAX],char a[10][10][4],int jobInd,int dataInd,int endInd)
+void run(string n[MAX],char a[10][10][4],int numInstr,int jobInd,int dataInd,int endInd)
 {
     int blockInd,reg_addr,instr_block = 0,addrs = 0,dataLine=dataInd;
     static char reg[4] = {'\0'};
-    int numInstr=20,itr=0;
-    while(itr<numInstr)
+    int itr=0,c=0;
+    while(true)
     {
-        //cout<<a[instr_block][addrs][0] <<" ";
+        if(a[instr_block][addrs][0] == 'H')
+            break;
+
         if(a[instr_block][addrs][0] == 'G')
         {
             blockInd = (a[instr_block][addrs][2] - '0') * 10 + (a[instr_block][addrs][3]- '0') ;
-            cout<<"|"<<blockInd<<"|G|";
             getdata(n,a,dataLine,endInd,blockInd);
             dataLine++;
         }
         else if(a[instr_block][addrs][0]  == 'P')
         {
             blockInd = (a[instr_block][addrs][2] - '0') * 10 + (a[instr_block][addrs][3]- '0') ;
-            cout<<"|"<<blockInd<<"|P|";
             putData(n,a,dataInd,endInd,blockInd);
         }
         else if(a[instr_block][addrs][0] == 'L')
         {
             reg_addr = (a[instr_block][addrs][2] - '0') * 10 + (a[instr_block][addrs][3]- '0') ;
             loadRegister(n,a,reg,reg_addr,dataInd,endInd);
-            //cout<<"Register content: "<<reg[0]<<reg[1]<<reg[2]<<reg[3]<<"\n";
         }
         else if(a[instr_block][addrs][0] == 'S')
         {
             reg_addr = (a[instr_block][addrs][2] - '0') * 10 + (a[instr_block][addrs][3]- '0') ;
             storeRegister(n,a,reg,reg_addr,dataInd,endInd);
         }
+        else if(a[instr_block][addrs][0] == 'C')
+        {
+            reg_addr = (a[instr_block][addrs][2] - '0') * 10 + (a[instr_block][addrs][3]- '0') ;
+            c = compareRegister(a,reg,reg_addr,dataInd,endInd,blockInd);
+        }
+        if(a[instr_block][addrs][0] == 'B' && c==1)
+        {
+            int temp_addrs = (a[instr_block][addrs][3]- '0');
+            int temp_instr = (a[instr_block][addrs][2] - '0');
+            addrs = temp_addrs;
+            instr_block = temp_instr;
+
+        }
         else
         {
-            cout<<"Nothing: "<<itr<<"Letter: "<< a[0][itr][0]<<"\n";
-        }
-        itr++;
-        addrs++;
+            itr++;
+            addrs++;
+                    if(addrs % 10 == 0)
+            {
+                instr_block++;
+                addrs = 0;
+            }}
 
-        if(itr % 10 == 0)
-        {
-            instr_block++;
-            addrs = 0;
-            //cout<<i<<"II"<<instrInd<<" IB "<<instr_block;
-        }
     }
-}
 
+}
+/*******************************************************MACHINE************************************************/
 void machine(string n[MAX],int len)
 {
     static char a[10][10][4]= {'0'};
@@ -165,13 +186,19 @@ void machine(string n[MAX],int len)
     //store instruction
     //instr_ block stores block number for storing instr
     int i=0,instrInd = jobidInd + 1,instr_block = 0,addrs = 0;
-    while(i <numInstr)
+    while(true)
     {
+        if(n[instrInd][k]== 'H')
+        {
+            a[instr_block][addrs][0] = n[instrInd][k];
+            break;}
         for(int j=0; j<4; j++)
         {
             a[instr_block][addrs][j] = n[instrInd][k];
             k++;
         }
+        cout<< a[instr_block][addrs] <<"<-" << n[instrInd] <<endl;
+        //cout<<" i "<<i;
         i++;
         addrs++;
 
@@ -183,14 +210,6 @@ void machine(string n[MAX],int len)
             k=0;
         }
     }
-
-    cout<<"Str: ";
-    for(int itr=0; itr < 10 ; itr++)
-        cout<<a[0][itr][0]<<" "<<a[0][itr][1]<<" "<<a[0][itr][2]<<" "<<a[0][itr][3]<<" "<<endl;
-    cout<<"Str: ";
-    for(int itr=0; itr < 10 ; itr++)
-        cout<<a[1][itr][0]<<" "<<a[1][itr][1]<<" "<<a[1][itr][2]<<" "<<a[1][itr][3]<<" "<<endl;
-
-    run(n,a,jobidInd,DataInd,EndInd);
+    run(n,a,numInstr,jobidInd,DataInd,EndInd);
 
 }
